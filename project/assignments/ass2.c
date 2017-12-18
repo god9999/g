4c66ef03b472f53263f86dcf1658c242c69f5688
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-18 21:08:46 +0530

    Create ass2.c

---
+++ project/assignments/ass2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+ int a;
+ printf("Enter the number");
+ scanf("%d",&a);
+ if(a%2==0)
+ {
+ printf("Positive");
+ }
+ else
+ {
+ printf("Negative");
+ }
+ return 0;
+ }
