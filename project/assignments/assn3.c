7c4e01f92c9a0bb7526a44fc77b42622ea221a0a
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-18 21:23:01 +0530

    Create assn3.c

---
+++ project/assignments/assn3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+  char ch;
+  printf("Enter the charcter");
+  scanf("%c",&ch);
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+    {
+       printf("vowel");
+     }
+    else
+    {
+      printf("consonant");
+     }
+  return 0;
+}
