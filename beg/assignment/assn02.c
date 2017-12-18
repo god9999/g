f385eed87236f90699898155dd46dc7dae794418
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-18 09:25:36 +0000

    Create assn02.c

---
+++ beg/assignment/assn02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+char c[]="Sachin Ramesh Tendulkar";
+char a[]="Little Master";
+int b=18426;
+float d=44.83;
+int e=463;
+printf("%s\n",c);
+printf("%s\n",a);
+printf("%d\n",b);
+printf("%.2f\n",d);
+printf("%d\n",e();
+return 0;
+}
