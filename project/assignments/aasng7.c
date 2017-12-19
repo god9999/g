5f2d3f4c11461dc3a7b55e8c6b1a4d4dc12475f6
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-19 07:38:30 +0530

    Create aasng7.c

---
+++ project/assignments/aasng7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+   int i,sum,n;
+   printf("Enter a number");
+   scanf("%d",&n);
+   for(i=0;i<=n;i++)
+   {
+     sum=sum+i;
+    }
+    printf("%d",sum);
+return 0;
+}
