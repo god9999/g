347af4508a629cd503e8bcf7e98b31b3b395f141
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-19 11:17:50 +0000

    Update evenloop.c

--- project/assignments/evenloop.c
+++ project/assignments/evenloop.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+  int a,b,i;
+  scanf("%d%d",&a,&b);
+  for(i=a;i<=20;i++)
+  {
+    if(i%2==0)
+    {
+       printf("%d\n",i);
+     }
+   }
+   return 0;
+}
