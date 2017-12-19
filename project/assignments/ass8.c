2a6b4624316df9198fc0dd7e8913487ebf8bc378
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-19 07:44:20 +0530

    Create ass8.c

---
+++ project/assignments/ass8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+      char a[]="Hello";
+      int i;
+      
+      for(i=0;i<5;i++)
+      {
+      	printf("%s\n",a);
+      }
+      return 0;
+}
