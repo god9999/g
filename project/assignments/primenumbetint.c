96ff656634ea3d5c194a30e06ec3c7dc936268e8
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-19 20:26:00 +0530

    Create primenumbetint.c

---
+++ project/assignments/primenumbetint.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+  int a,b,c,i,j;
+  scanf("%d%d",&a,&b);
+  for(i=a;i<=b;i++)
+  {
+  int c=0;
+    for(j=2;j<=i/2;j++)
+     {
+       if(i%j==0)
+       {
+          c++;
+          break;
+        }
+      }
+  if(c==0)
+  {
+    printf("%d",i);
+  }
+  }
+  return 0;
+  }
