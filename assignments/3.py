709704e8ac23e64823c07341dcff61d500b1560f
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-27 01:12:53 +0530

    Update 3.py

--- assignments/3.py
+++ assignments/3.py
@@ -1,26 +1,22 @@
-employee_id = 1001
-basic_salary = 15000.00
-allowances=6000.00
-bill_amount = 0.0
-gross_salary=0
-net_salary
-print("Bill Id: %d" %bill_id)
-print("Customer Id: %d" %customer_id)
-print("Bill Amount:Rs. %f" %bill_amount)
-if bill_amount <5000:
-	income_tax=0
-elif ((bill_amount >=5001) and (bill_amount <=10000):
-	income_tax=0.1
-elif ((bill_amount >=10001) and (bill_amount <=20000)):
-	income_tax=0.2
-elif bill_amont>20000:
-	income_tax=0.3
+e=1001
+bs=15000.00
+al=6000.00
+gs=ns=it=0
+if bs <= 5000:
+	it=0
+elif bs >=5001 and bs <=10000:
+	it=10/100
+elif bs >=10001 and bs <=20000:
+	it=20/100
 else:
-	income_tax=0
-gross_salary=basic_salary+allowances
-net_salary=gross_salary-income_tax
-print(employee_id)
-print(basic_salary)
-print(allowances)
-print(gross_salary)
-print(income_tax)
+	it=30/100
+	
+  gs=bs+al
+  ns=gs-it
+  
+print(e)
+print(bs)
+print(al)
+print(gs)
+print(ns)
+print(it)
