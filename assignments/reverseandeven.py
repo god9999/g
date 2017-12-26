0c22661edececee836dfaa217d4f7f72c3012ad2
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-27 02:15:59 +0530

    Create reverseandeven.py

---
+++ assignments/reverseandeven.py
@@ -0,0 +1,13 @@
+str1= str(input("enter the string"))
+for i in range( 0 , 30 ):
+	if str1[0]%2 ==0:
+         	  print(str1)
+          rev_str = reversed(str1)
+          
+if(list(str1) == list(rev_list)):
+	print(str1)
+else:
+	print("zero")
+            
+               
+       
