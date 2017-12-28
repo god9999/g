437cdb55da4155373a6877fdb7204b9565d570ff
god9999 <32997611+god9999@users.noreply.github.com>
2017-12-28 14:32:09 +0530

    Update assng3.c

--- hunter/assignment/assng3.c
+++ hunter/assignment/assng3.c
@@ -1,1 +1,40 @@
 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int max(int[],int);
+int main(void)
+{
+    int ai,i;
+    int result;
+    int arr[10];
+    cout<<"Enter the value";
+    cin>>ai;
+    cout<<"enter the array variables";
+    for(i=0;i<ai;i++)
+    {
+        cin>>arr[i];
+    }
+    result = max(arr,ai);
+    cout<<result;
+    return 0;
+}
+int max(int arr[],int a)
+{
+    int max;
+    int i;
+    max=arr[0];
+    for(i=0;i<a;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    return max;
+}
+        
+
+  
+
+
