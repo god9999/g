5a00ea70774b3c6dbbefe63a367098a70c3ec6a9
god9999 <32997611+god9999@users.noreply.github.com>
2018-01-02 09:42:59 +0530

    Update capitalletter.py

--- assignments/capitalletter.py
+++ assignments/capitalletter.py
@@ -2,6 +2,5 @@ string=raw_input("Enter string:")
 count=0
 for i in string:
       if(i.isupper()):
-            count=count+1
-print("The number of lowercase characters is:")
+            count++;
 print(count)
