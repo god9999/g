97deb91c7ac417371410bdc550553fc203dacdd5
god9999 <32997611+god9999@users.noreply.github.com>
2018-01-08 21:36:58 +0530

    Create 123.c

---
+++ assignments/123.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+    char str[20];
+    printf("Enter the string");
+    gets(str);
+    int length=strlen(str);
+    for(int i=0;i<length;i=i+2)
+    {
+    	if(str[i]!='\0')
+    	{
+    		int temp;
+    		temp=str[i];
+    		str[i]=str[i+1];
+    		str[i+1]=temp;
+    	}
+    }
+    printf("%s",str);
+    return 0;
+}
