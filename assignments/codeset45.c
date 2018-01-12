ac555e8a802dee4ac03abb9a420d48eba78652b2
god9999 <32997611+god9999@users.noreply.github.com>
2018-01-12 20:23:56 +0530

    Create codeset45.c

---
+++ assignments/codeset45.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+ {
+  char s[200];
+ scanf("%s",s);
+  int i;      
+  int num = 0;
+  for(i=0;i<strlen(s);i++) 
+  {
+      if(!(strlen(s)=='\0'))
+      {
+          num++;
+      }
+  }
+  printf("%d",num);
+  return 0;
+  }
+
+	     
+
+	  
+
+	   
+
+	    
+
+	 
+
