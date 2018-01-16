264070597c2e52a784ff4871cbc501d22526e261
god9999 <32997611+god9999@users.noreply.github.com>
2018-01-16 11:44:02 +0530

    Update codeset57.c

--- assignments/codeset57.c
+++ assignments/codeset57.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main(void)
 {
-	int a;
-	int temp,result;
+	long int a,temp;
+	int result;
 	scanf("%d",&a);
 	temp=a;
 	while(temp!=0)
 	{
-		temp=temp%10;
-		result=result*10+temp;
 		temp=temp/10;
+		result++;
 	}
 	printf("%d",result);
 	return 0;
