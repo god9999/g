bc13e8bce3a723d95eb5fe53106080b7e619fc43
god9999 <32997611+god9999@users.noreply.github.com>
2018-01-19 20:47:40 +0530

    Create codeset114.c

---
+++ assignments/codeset114.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+  int a,i;
+  int arr[100],min;
+  scanf("%d",&a);
+  for(i=0;i<a;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  min=arr[0];
+  for(i=0;i<a;i++)
+  {
+      if(arr[i]<min)
+      {
+          min=arr[i];
+      }
+  }
+  printf("%d",min);
+  return 0;
+}
