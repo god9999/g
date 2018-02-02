c43c8686f1912111445374ea94069bfff6985770
god9999 <32997611+god9999@users.noreply.github.com>
2018-02-02 19:41:29 +0530

    Create set7code3.c

---
+++ assignments/set7code3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+    char str[20];
+    int length;
+    int i;
+    int count;
+    printf("Enter the string\n");
+    gets(str);
+    length=strlen(str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' ')
+        {
+            count++;
+        }
+    }
+    printf("%d",count+1);
+
+  
+    return 0;
+}
