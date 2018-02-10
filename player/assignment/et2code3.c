3496f63b4cb75b24dc70a447b2b1d5afdba93517
god9999 <32997611+god9999@users.noreply.github.com>
2018-02-10 12:30:25 +0530

    Create et2code3.c

---
+++ player/assignment/et2code3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int  main()
+{
+int num, temp, digit, sum = 0;
+    scanf("%d", &num);
+    temp = num;
+    while (num > 0)
+    {
+        digit = num % 10;
+        sum  = sum + digit*digit;
+        num=num/ 10;
+    }
+    printf("%d",sum);
+}
