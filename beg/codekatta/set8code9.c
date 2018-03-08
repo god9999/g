0f7a95410e5173745c94f085ce95b3a5b78cd826
god9999 <32997611+god9999@users.noreply.github.com>
2018-03-08 23:09:58 +0530

    Create set8code9.c

---
+++ beg/codekatta/set8code9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,c;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+  c=a*b;
+  for(i=0;i<c;i++)
+  {
+    if(c==i*i)
+    {
+printf("yes");
+return 0;
+}
+}
+printf("no");
+}
