2c4aa71b96ef247a8a378677a04dfe4dc35711a3
god9999 <32997611+god9999@users.noreply.github.com>
2018-03-08 22:50:07 +0530

    Create set8code7.c

---
+++ beg/codekatta/set8code7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+    int a;
+    scanf("%d",&a);
+    if(a%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
