8436c78813510e14712fd22a51ab21ae32199ba5
god9999 <32997611+god9999@users.noreply.github.com>
2018-03-09 01:05:40 +0530

    Create set10code.c

---
+++ beg/codekatta/set10code.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+ 
+int main(void) 
+{
+	int n,array[30],i;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&array[i]);
+	}
+	for(i=1;i<=n;i++)
+	{
+		if(i!=array[i])
+		{
+			printf("%d",i);
+			break;
+		}
+	}
+ 
+	return 0;
+}
