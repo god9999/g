a7dd823b242bcdaa6ce2ec26c7232f2a2633585a
god9999 <32997611+god9999@users.noreply.github.com>
2018-06-22 12:28:22 +0530

    Create mat.java

---
+++ hunter/assignment/mat.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.math.*;
+class solution{
+    public static void main(String aa[]){
+        int l,k;
+       Scanner s=new Scanner(System.in);
+       int n=s.nextInt();
+       int m=s.nextInt();
+       int a[][]=new int[n][m];
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               a[i][j]=s.nextInt();
+           }
+       }
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               if(a[i][j]==0){
+                   for( k=0;k<m;k++){
+                       a[i][k]=0;
+                   }
+                   for(l=0;l<n;l++){
+                       a[l][j]=0;
+                   }
+               }
+           }
+       }
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               System.out.println(a[i][j]);
+           }
+           
+       }
+    }
+}
